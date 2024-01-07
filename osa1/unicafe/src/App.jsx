8c076a48c3b1bca603d3aa3a76aac804e9358be2
Iisakki Säkkinen <esakkinen@topcon.com>
2024-01-07 21:27:04 +0200

    unicafe step1 valmis

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -7,7 +7,16 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
